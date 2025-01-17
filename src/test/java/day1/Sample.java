53477fdc9acdf1765628f3b0e697a191945636b8
aravindanath <20963786+aravindanath@users.noreply.github.com>
2025-01-17 08:13:42 +0530

    added new line of code

--- src/test/java/day1/Sample.java
+++ src/test/java/day1/Sample.java
@@ -3,5 +3,7 @@ package day1;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Happy learning Automation");
     }
 }
