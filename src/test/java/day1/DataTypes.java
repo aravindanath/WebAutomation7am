6aa59e6a441e590b8d70c04ab27cfd0b9ed4d297
aravindanath <20963786+aravindanath@users.noreply.github.com>
2025-01-17 07:48:52 +0530

    Adding new files

---
+++ src/test/java/day1/DataTypes.java
@@ -0,0 +1,30 @@
+package day1;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        int age;
+
+        int myAge = 23;
+        System.out.println("My Age: "+myAge);
+        short pinCode = 12345;
+        System.out.println("Pin Code: "+pinCode);
+        long mobileNumber = 12345678901L;
+        System.out.println("Mobile Number: "+mobileNumber);
+        float salary = 12345.33f;
+        System.out.println("Salary: "+salary);
+        double price = 123456.789;
+        System.out.println("Price: "+price);
+        char ch = '9';
+        System.out.println("Char: "+ch);
+        boolean isJavaFun = true;
+        System.out.println("Is Java Fun: "+isJavaFun);
+
+        String name = "John23453";
+
+        age = 33;
+        System.out.println("Age: "+age);
+
+    }
+
+}
---
+++ src/test/java/day1/Operators1.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class Operators1 {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+        int sum = x + y;
+        System.out.println("Sum: "+sum);
+
+        int z = 10;
+        z++;
+        System.out.println("Z: "+z);
+        x =20;
+        x--;
+        System.out.println("X: "+x);
+
+    }
+}
