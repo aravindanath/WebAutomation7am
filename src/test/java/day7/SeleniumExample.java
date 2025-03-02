e3e42a5965dc0ae3bd09e868a9432f982e9fa85b
aravindanath <20963786+aravindanath@users.noreply.github.com>
2025-03-02 13:32:09 +0530

    added day 2 code string conditions part-1

--- src/test/java/day7/SeleniumExample.java
+++ src/test/java/day7/SeleniumExample.java
@@ -3,6 +3,7 @@ package day7;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
@@ -11,24 +12,27 @@ public class SeleniumExample {
 
     public static void main(String[] args) {
 
-        String browser = Utitls.getValue("browser"), url = Utitls.getValue("stage_Url");
+        String browser = Utitls.getValue("browser"), url = Utitls.getValue("url");
         WebDriver driver = null;
         if(browser.equalsIgnoreCase("chrome")){
-              driver = new ChromeDriver();
+            ChromeOptions ops = new ChromeOptions();
+            ops.addArguments("--headless");
+              driver = new ChromeDriver(ops);
         }else if(browser.equalsIgnoreCase("firefox")) {
               driver = new FirefoxDriver();
         }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
         driver.get(url);
+        System.out.println(driver.getTitle());
 
-        try {
-            driver.findElement(By.xpath("//a[text()='Skip to Main Content']")).click();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        driver.findElement(By.xpath("(//a[text()='BUSINESS'])[1]")).click();
+//        try {
+//            driver.findElement(By.xpath("//a[text()='Skip to Main Content']")).click();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        driver.findElement(By.xpath("(//a[text()='BUSINESS'])[1]")).click();
         driver.quit();
     }
 }
